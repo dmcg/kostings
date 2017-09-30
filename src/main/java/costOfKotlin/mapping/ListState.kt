745da8d7cc11e29ad7d7c910412c359f0cbbe2dd
Duncan McGregor <duncan@oneeyedmen.com>
2017-09-30 14:46:02 +0100

    More results for mapping over lists

--- src/main/java/costOfKotlin/mapping/ListState.kt
+++ src/main/java/costOfKotlin/mapping/ListState.kt
@@ -2,8 +2,10 @@ package costOfKotlin.mapping
 
 import org.openjdk.jmh.annotations.Scope
 import org.openjdk.jmh.annotations.State
+import java.util.*
 
 @State(Scope.Thread)
 open class ListState {
-    val listOfStrings = List(10000, Int::toString)
+    val arrayListOfStrings = List(10000, Int::toString)
+    val linkedListOfStrings = LinkedList(arrayListOfStrings)
 }
--- src/main/java/costOfKotlin/mapping/Mapping.kt
+++ src/main/java/costOfKotlin/mapping/Mapping.kt
@@ -10,7 +10,7 @@ open class Mapping {
 
     @Benchmark
     fun baseline(listState: ListState) : List<String> {
-        val list = listState.listOfStrings
+        val list = listState.arrayListOfStrings
         val result = ArrayList<String>(list.size)
         for (i in 0 until result.size) {
             result.add(list.get(i))
@@ -20,7 +20,7 @@ open class Mapping {
 
     @Benchmark
     fun baseline_iterator(listState: ListState) : List<String> {
-        val list = listState.listOfStrings
+        val list = listState.arrayListOfStrings
         val result = ArrayList<String>(list.size)
         val iterator = list.iterator()
         while (iterator.hasNext()) {
@@ -31,17 +31,37 @@ open class Mapping {
 
     @Benchmark
     fun map_identity(listState: ListState) : List<String> {
-        return listState.listOfStrings.map { it }
+        return listState.arrayListOfStrings.map { it }
     }
 
     @Benchmark
     fun map_identityFunction(listState: ListState) : List<String> {
-        return listState.listOfStrings.map(identity())
+        return listState.arrayListOfStrings.map(identity())
     }
 
     @Benchmark
-    fun specialised_map_identity(listState: ListState) : List<String> {
-        return listState.listOfStrings.listMap { it }
+    fun indexedMap(listState: ListState) : List<String> {
+        return listState.arrayListOfStrings.indexedMap { it }
+    }
+
+    @Benchmark
+    fun indexedMap_on_linked_list(listState: ListState) : List<String> {
+        return listState.linkedListOfStrings.indexedMap { it }
+    }
+
+    @Benchmark
+    fun specialisedMap(listState: ListState) : List<String> {
+        return listState.arrayListOfStrings.specialisedMap { it }
+    }
+
+    @Benchmark
+    fun specialisedMap_on_linked_list(listState: ListState) : List<String> {
+        return listState.linkedListOfStrings.specialisedMap { it }
+    }
+
+    @Benchmark
+    fun map_on_linked_list(listState: ListState) : List<String> {
+        return listState.linkedListOfStrings.map{ it }
     }
 
     @Test
@@ -52,17 +72,24 @@ open class Mapping {
 
     @Test
     fun `we could do so much better`() {
-        assertThat(this::baseline, ! probablyDifferentTo(this::specialised_map_identity))
+        assertThat(this::baseline, ! probablyFasterThan(this::specialisedMap, byAFactorOf = 0.05))
+        assertThat(this::map_on_linked_list, ! probablyDifferentTo(this::specialisedMap_on_linked_list))
     }
+
 }
 
 @Suppress("NOTHING_TO_INLINE")
 inline fun<T> identity(): (T) -> T = { it }
 
-inline fun <T, R> List<T>.listMap(transform: (T) -> R): List<R> {
+inline fun <T, R> List<T>.indexedMap(transform: (T) -> R): List<R> {
     val result = ArrayList<R>(this.size)
     for (i in 0 until result.size) {
         result.add(transform(this.get(i)))
     }
     return result
+}
+
+inline fun <T, R> List<T>.specialisedMap(transform: (T) -> R): List<R> = when {
+    this is RandomAccess -> indexedMap(transform)
+    else -> map(transform)
 }
