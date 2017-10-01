3dee4b733e09c7bc1b23269b768061ac0a3f1c76
Duncan McGregor <duncan@oneeyedmen.com>
2017-10-01 12:54:46 +0100

    Implement checking of results for reasonableness

--- src/main/java/com/oneeyedmen/kostings/checking.kt
+++ src/main/java/com/oneeyedmen/kostings/checking.kt
@@ -2,6 +2,36 @@ package com.oneeyedmen.kostings
 
 import org.apache.commons.math3.stat.inference.TestUtils.oneWayAnovaTest
 
+object DataChecking{
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        readBatches(canonicalResultsDir).forEach { batch ->
+            reportIffness(batch)
+        }
+        readBatches(resultsDir).forEach { batch ->
+            reportIffness(batch)
+        }
+    }
+
+    private fun reportIffness(batch: Batch) {
+        val iffyness: Map<Result, String> = batch.results.associate { it to isIffy(it.stats.values) }
+        println("${batch.dataFile} : ${iffyness.summary()}")
+        iffyness.forEach { result, iffy ->
+            println("    ${result.benchmarkName} : ${iffy}")
+        }
+        println()
+    }
+
+}
+
+private fun Map<Result, String>.summary(): String = this.values.find { it != "ok" } ?: "ok"
+
+private fun isIffy(values: DoubleArray): String =
+    when {
+        values.size < 50 -> "too small"
+        else -> if (hasAnomalousData(values)) "iffy" else "ok"
+    }
 
 fun hasAnomalousData(data: DoubleArray, alpha: Double = 0.01, chunkSize: Int = 50): Boolean {
     // can you reject the hypothesis that the chunks are the same w.r.t mean, and only have an alpha% chance of being wrong?
