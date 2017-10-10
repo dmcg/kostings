dedbe2c35b50d2bf51ec9a185fc551da5a3be421
jsn <john@stigmergist.com>
2017-10-10 18:35:36 +0100

    Rewokred D3JS comparative stats to be more consistent and added extra info to aid in debugging comparisons

--- src/main/java/com/oneeyedmen/kostings/testing.kt
+++ src/main/java/com/oneeyedmen/kostings/testing.kt
@@ -7,7 +7,9 @@ import com.oneeyedmen.kostings.matchers.Stats
 import com.oneeyedmen.kostings.matchers.probablyDifferentTo
 import com.oneeyedmen.kostings.matchers.probablyLessThan
 import com.oneeyedmen.kostings.matchers.probablyMoreThan
+import org.apache.commons.math3.distribution.TDistribution
 import org.apache.commons.math3.random.EmpiricalDistribution
+import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics
 import org.junit.internal.RealSystem
 import org.junit.internal.TextListener
 import org.junit.runner.JUnitCore
@@ -40,15 +42,15 @@ fun runTests(vararg testClasses: Class<*>): org.junit.runner.Result =
 
 
 fun probablyDifferentTo(benchmarkFunction: KFunction<*>, alpha: Double = 0.05) =
-    benchmarkMatcher(benchmarkFunction) { probablyDifferentTo(it, alpha) }
+    benchmarkMatcher(benchmarkFunction, alpha) { probablyDifferentTo(it, alpha) }
 
 fun probablyFasterThan(benchmarkFunction: KFunction<*>, byAFactorOf: Double = 0.0, alpha: Double = 0.05) =
-    benchmarkMatcher(benchmarkFunction) { probablyMoreThan(it, byAFactorOf, alpha) }
+    benchmarkMatcher(benchmarkFunction, alpha) { probablyMoreThan(it, byAFactorOf, alpha) }
 
 fun probablySlowerThan(benchmarkFunction: KFunction<*>, byAFactorOf: Double = 0.0, alpha: Double = 0.05) =
-    benchmarkMatcher(benchmarkFunction) { probablyLessThan(it, byAFactorOf, alpha) }
+    benchmarkMatcher(benchmarkFunction, alpha) { probablyLessThan(it, byAFactorOf, alpha) }
 
-private fun benchmarkMatcher(benchmarkFunction: KFunction<*>, comparator: (Stats) -> Matcher<Stats>) =
+private fun benchmarkMatcher(benchmarkFunction: KFunction<*>, alpha : Double, comparator: (Stats) -> Matcher<Stats>) =
     object : Matcher<KFunction<*>> {
         override val description get() = delegateMatcher.description
 
@@ -56,7 +58,7 @@ private fun benchmarkMatcher(benchmarkFunction: KFunction<*>, comparator: (Stats
             val actualResult = resultFor(actual)
             val matcher = delegateMatcher.invoke(actualResult)
             if (matcher is MatchResult.Mismatch) {
-                return MatchResult.Mismatch(matcher.description+"\n(visual comparison can be found at "+ dumpComparison(actualResult,resultFor(benchmarkFunction))+" )")
+                return MatchResult.Mismatch(matcher.description+"\n(visual comparison can be found at "+ dumpComparison(actualResult,resultFor(benchmarkFunction), alpha)+" )")
             }
             else
                 return matcher
@@ -73,20 +75,34 @@ val KFunction<*>.methodName
         "${(it.owner as KClass<*>).jvmName}.${it.name}"
     } ?: throw IllegalArgumentException()
 
-private fun dumpComparison(result1: Result, result2: Result) : String {
+private fun dumpComparison(result1: Result, result2: Result, alpha: Double = 0.05) : String {
     val template = Testing::class.java.getResource("/template.html")
     val renderJS = Testing::class.java.getResource("/render.js")
     val d3js = Testing::class.java.getResource("/d3.v4.min.js")
+    val stylesheet = Testing::class.java.getResource("/stylesheet.css")
     var outputText = template.readText()
-    outputText=outputText.replace("%%D3%%",d3js.path).replace("%%RENDER%%",renderJS.path)
-    outputText=outputText.replace("%%TITLE%%", result1.benchmarkName+" vs "+ result2.benchmarkName).replace("%%GENERATED%%", Date().toString())
+    outputText=outputText.replace("%%D3%%",d3js.path).replace("%%RENDER%%",renderJS.path).replace("%%STYLESHEET%%",stylesheet.path)
+    outputText=outputText.replace("%%RESULT1_NAME%%", result1.benchmarkName)
+    outputText=outputText.replace("%%RESULT2_NAME%%", result2.benchmarkName)
+    outputText=outputText.replace("%%GENERATED%%", Date().toString())
 
     outputText=outputText.replace("%%RESULT1%%", jacksonObjectMapper().writeValueAsString(result1.histogram()))
     outputText=outputText.replace("%%RESULT2%%",jacksonObjectMapper().writeValueAsString(result2.histogram()))
     outputText=outputText.replace("%%MEAN1%%", result1.data.mean.toString())
     outputText=outputText.replace("%%MEAN2%%", result2.data.mean.toString())
+    outputText=outputText.replace("%%LOWERCI1%%", (result1.data.mean-result1.confidenceInterval(alpha)).toString())
+    outputText=outputText.replace("%%UPPERCI1%%", (result1.data.mean+result1.confidenceInterval(alpha)).toString())
+    outputText=outputText.replace("%%LOWERCI2%%", (result2.data.mean-result2.confidenceInterval(alpha)).toString())
+    outputText=outputText.replace("%%UPPERCI2%%", (result2.data.mean+result2.confidenceInterval(alpha)).toString())
 
     outputText=outputText.replace("%%UNITS%%", result1.units)
+    outputText=outputText.replace("%%ALPHA%%", alpha.toString())
+    val meanDiffCI =meanDiffCI(result1.data,result2.data,alpha)
+    val meanDiff = result2.data.mean-result1.data.mean
+    val lowerMeanCI = meanDiff - meanDiffCI
+    outputText=outputText.replace("%%DIFFLOWERCI%%", lowerMeanCI.toString())
+    val upperMeanCI = meanDiff + meanDiffCI
+    outputText=outputText.replace("%%DIFFUPPERCI%%", upperMeanCI.toString())
 
     val file = createTempFile(suffix = ".html")
     file.writeText(outputText)
@@ -94,7 +110,36 @@ private fun dumpComparison(result1: Result, result2: Result) : String {
 }
 
 fun Result.histogram() : List<Array<Double>> {
-    val bucketStats = EmpiricalDistribution(31)
+    val binCount = 21
+    val bucketStats = EmpiricalDistribution(binCount)
     bucketStats.load(data.values)
-    return bucketStats.getBinStats().map { arrayOf(it.mean, it.n.toDouble()) }
+    val upperBounds = bucketStats.upperBounds
+    val min = data.values.min()!!
+    var lastBound = min
+    val result = mutableListOf<Array<Double>>()
+    for ( (index, summaryStatistics) in bucketStats.getBinStats().asIterable().withIndex()) {
+        val centre = ((upperBounds[index]-lastBound) /2.0) + lastBound
+        lastBound = upperBounds[index]
+        result.add(arrayOf(centre,summaryStatistics.n.toDouble()))
+    }
+    return result
+}
+
+fun Result.confidenceInterval(alpha: Double = 0.05) : Double {
+    val tDist = TDistribution(data.n - 1.0)
+    val a = tDist.inverseCumulativeProbability(1.0 - alpha / 2)
+    return a * data.standardDeviation / Math.sqrt(data.n.toDouble())
+}
+
+private fun meanDiffCI(stats1 : DescriptiveStatistics, stats2 : DescriptiveStatistics, alpha: Double = 0.05): Double {
+    val v1 = stats1.variance
+    val n1 = stats1.n
+    val v2 = stats2.variance
+    val n2 =stats2.n
+    val df = (v1 / n1 + v2 / n2) * (v1 / n1 + v2 / n2) / (v1 * v1 / (n1 * n1 * (n1 - 1.0)) + v2 * v2 / (n2 * n2 * (n2 - 1.0)))
+    val tDist = TDistribution(df-1)
+    val t = tDist.inverseCumulativeProbability(1.0 - alpha / 2)
+    val mse = Math.sqrt( v1/n1 + v2/n2)
+    val ci = t*mse
+    return ci
 }
--- src/main/resources/render.js
+++ src/main/resources/render.js
@@ -5,7 +5,7 @@ var margin = {top: 20, right: 150, bottom: 30, left: 50},
 var x = d3.scaleLinear().range([0, width]);
 var y = d3.scaleLinear().range([height, 0]);
 
-var svg = d3.select("body").append("svg")
+var svg = d3.select("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
   .append("g")
@@ -33,7 +33,7 @@ var svg = d3.select("body").append("svg")
   //Add Result1
   svg.append("path")
     .datum(result1)
-    .attr("class", "result1")
+    .attr("class", "histogram result1")
     .attr("d", d3.line()
                  .curve(d3.curveStep)
                  .x(function(d) { return x(d[0]); })
@@ -43,7 +43,7 @@ var svg = d3.select("body").append("svg")
     //Add Result2
     svg.append("path")
       .datum(result2)
-      .attr("class", "result2")
+      .attr("class", "histogram result2")
       .attr("d", d3.line()
                    .curve(d3.curveStep)
                    .x(function(d) { return x(d[0]); })
@@ -52,19 +52,33 @@ var svg = d3.select("body").append("svg")
 
   //Add Mean 1
   svg.append("svg:line")
-      .attr("class", "mean1")
+      .attr("class", "mean result1")
       .attr("x1", x(mean1))
       .attr("y1", height)
       .attr("x2", x(mean1))
-      .attr("y2", -10);
+      .attr("y2", 0);
+
+  svg.append("svg:rect")
+      .attr("class", "ci1")
+      .attr("x", x(lowerCI1))
+      .attr("y", 0)
+      .attr("width", x(upperCI1)-x(lowerCI1))
+      .attr("height", height);
 
     //Add Mean 2
     svg.append("svg:line")
-        .attr("class", "mean2")
+        .attr("class", "mean result2")
         .attr("x1", x(mean2))
         .attr("y1", height)
         .attr("x2", x(mean2))
-        .attr("y2", -10);
+        .attr("y2", 0);
+
+  svg.append("svg:rect")
+      .attr("class", "ci2")
+      .attr("x", x(lowerCI2))
+      .attr("y", 0)
+      .attr("width", x(upperCI2)-x(lowerCI2))
+      .attr("height", height);
 
   svg.append("text")
       .attr("class", "x label")
@@ -81,3 +95,34 @@ var svg = d3.select("body").append("svg")
       .attr("transform", "rotate(-90)")
       .text("count");
 
+var tableFormat = d3.format(".5s");
+d3.select("#mean1").text(tableFormat(mean1));
+d3.select("#lowerCI1").text(tableFormat(lowerCI1));
+d3.select("#upperCI1").text(tableFormat(upperCI1));
+d3.select("#mean2").text(tableFormat(mean2));
+d3.select("#lowerCI2").text(tableFormat(lowerCI2));
+d3.select("#upperCI2").text(tableFormat(upperCI2));
+d3.select("#difflowerCI").text(tableFormat(difflowerCI));
+d3.select("#diffupperCI").text(tableFormat(diffupperCI));
+
+var ciFormat = d3.format(".3s");
+var confidence = ciFormat((1-alpha)*100);
+d3.select("#alpha").text("( "+confidence+"% confidence )");
+
+var pctFormat = d3.format(".2f");
+if (difflowerCI<0 && diffupperCI>0)
+    d3.select("#analysis").text("As this range contains 0, these result sets can be said to have the same mean with "+confidence+"% confidence");
+else {
+    d3.select("#analysis").text("These result sets can be said to have different means, with "+confidence+"% confidence they are not the same");
+    if (diffupperCI<0) {
+        d3.select("#direction").text("less");
+        d3.select("#lowerfactor").text(pctFormat(-diffupperCI*100.0/mean1)+"%");
+        d3.select("#upperfactor").text(pctFormat(-difflowerCI*100.0/mean1)+"%");
+    }
+    else {
+        d3.select("#direction").text("greater");
+        d3.select("#lowerfactor").text(pctFormat(difflowerCI*100.0/mean1)+"%");
+        d3.select("#upperfactor").text(pctFormat(diffupperCI*100.0/mean1)+"%");
+    }
+    d3.select("#subanalysis").style("visibility","visible");
+}
