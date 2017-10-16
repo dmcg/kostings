636f08e565bddaf2b90594c313c8ed94eb09d2c6
Duncan McGregor <duncan@oneeyedmen.com>
2017-10-16 18:19:56 +0100

    Survive empty json files when we resurrect batches

--- src/main/java/com/oneeyedmen/kostings/resurrecting.kt
+++ src/main/java/com/oneeyedmen/kostings/resurrecting.kt
@@ -1,11 +1,15 @@
 package com.oneeyedmen.kostings
 
 import java.io.File
+import java.io.FilenameFilter
 
 fun readBatches(dir: File): Sequence<Batch> = dir
-    .listFiles { _, name -> name.endsWith(".json") }
+    .listFiles(isAResultFile)
     .asSequence()
     .mapNotNull { Batch.readFromJson(it) }
 
 fun readResults(dir: File): Sequence<IndividualBenchmarkResult> = readBatches(dir).flatMap { it.results.asSequence() }
 
+private val isAResultFile: FilenameFilter = FilenameFilter { dir, name ->
+    name.endsWith(".json") && dir.resolve(name).length() >= 0 // aborted runs leave behind 0 length files
+}
