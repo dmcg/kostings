f7e5b8f900512b10b7717c4f7a4cabf168910ac4
Duncan McGregor <duncan@oneeyedmen.com>
2017-10-16 18:20:54 +0100

    Slightly better temp filenames

--- src/main/java/com/oneeyedmen/kostings/plotting.kt
+++ src/main/java/com/oneeyedmen/kostings/plotting.kt
@@ -4,10 +4,10 @@ import java.io.File
 import java.util.concurrent.TimeUnit
 
 fun ResultSet.plotHistogramTo(outputFile: File) =
-    runPython("plot-histogram.py", tempFile(description, ".csv", this::writeStatsCSV), outputFile)
+    runPython("plot-histogram.py", tempFile(description + "-", ".csv", this::writeStatsCSV), outputFile)
 
 fun Batch.plotSamplesTo(outputFile: File) =
-    runPython("plot-samples.py", tempFile(description, ".samples.csv", this::writeSamplesCSV), outputFile)
+    runPython("plot-samples.py", tempFile(description + "-", ".samples.csv", this::writeSamplesCSV), outputFile)
 
 fun Batch.plotIn(outputDir: File) {
     plotHistogramTo(outputDir.resolve(batchOptions.outputFilename + ".png"))
@@ -21,7 +21,7 @@ fun Iterable<Batch>.plotIn(outputDir: File) =
 
 
 private fun runPython(script: String, input: File, outputFile: File) {
-    ProcessBuilder("python", script, "\"${input.absolutePath}\"", "\"${outputFile.absolutePath}\"")
+    ProcessBuilder("python", script, "${input.absolutePath}", "${outputFile.absolutePath}")
         .inheritIO()
         .start()
         .waitFor(5, TimeUnit.SECONDS)
