a77df7990e8a27ca5bcce51bb547543dc595972c
Duncan McGregor <duncan@oneeyedmen.com>
2017-10-17 13:36:05 +0100

    Introduce typeAlias for BenchmarkFunction

--- src/main/java/com/oneeyedmen/kostings/testing.kt
+++ src/main/java/com/oneeyedmen/kostings/testing.kt
@@ -13,20 +13,22 @@ import kotlin.reflect.jvm.jvmName
 
 val resurrectedBatches by lazy { Results(Directories.canonicalResultsDir) }
 
-fun probablyDifferentTo(benchmarkFunction: KFunction<*>, alpha: Double = 0.05) =
+typealias BenchmarkFunction = KFunction<*>
+
+fun probablyDifferentTo(benchmarkFunction: BenchmarkFunction, alpha: Double = 0.05) =
     benchmarkMatcher(benchmarkFunction, alpha) { probablyDifferentTo(it, alpha) }
 
-fun probablyFasterThan(benchmarkFunction: KFunction<*>, byAFactorOf: Double = 0.0, alpha: Double = 0.05) =
+fun probablyFasterThan(benchmarkFunction: BenchmarkFunction, byAFactorOf: Double = 0.0, alpha: Double = 0.05) =
     benchmarkMatcher(benchmarkFunction, alpha) { probablyMoreThan(it, byAFactorOf, alpha) }
 
-fun probablySlowerThan(benchmarkFunction: KFunction<*>, byAFactorOf: Double = 0.0, alpha: Double = 0.05) =
+fun probablySlowerThan(benchmarkFunction: BenchmarkFunction, byAFactorOf: Double = 0.0, alpha: Double = 0.05) =
     benchmarkMatcher(benchmarkFunction, alpha) { probablyLessThan(it, byAFactorOf, alpha) }
 
-private fun benchmarkMatcher(benchmarkFunction: KFunction<*>, alpha : Double, comparator: (Stats) -> Matcher<Stats>) =
-    object : Matcher<KFunction<*>> {
+private fun benchmarkMatcher(benchmarkFunction: BenchmarkFunction, alpha : Double, comparator: (Stats) -> Matcher<Stats>) =
+    object : Matcher<BenchmarkFunction> {
         override val description get() = delegateMatcher.description
 
-        override fun invoke(actual: KFunction<*>): MatchResult {
+        override fun invoke(actual: BenchmarkFunction): MatchResult {
             val actualResult = resultFor(actual)
             val matcher = delegateMatcher.invoke(actualResult)
             if (matcher is MatchResult.Mismatch) {
@@ -39,10 +41,10 @@ private fun benchmarkMatcher(benchmarkFunction: KFunction<*>, alpha : Double, co
         private val delegateMatcher by lazy { comparator(resultFor(benchmarkFunction)) }
     }
 
-private fun resultFor(method: KFunction<*>) =
+private fun resultFor(method: BenchmarkFunction) =
     method.methodName.let { resurrectedBatches.resultNamed(it) } ?: throw IllegalStateException("no results were found for ${method.methodName}")
 
-val KFunction<*>.methodName
+val BenchmarkFunction.methodName
     get() = (this as? FunctionReference)?.let {
         "${(it.owner as KClass<*>).jvmName}.${it.name}"
     } ?: throw IllegalArgumentException()
