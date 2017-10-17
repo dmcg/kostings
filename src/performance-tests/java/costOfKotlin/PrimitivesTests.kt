cfe05f5a74b4a37c50a3783e8de3afe957f1c54d
Duncan McGregor <duncan@oneeyedmen.com>
2017-10-17 20:56:35 +0100

    Make more assertions for new data

--- src/performance-tests/java/costOfKotlin/PrimitivesTests.kt
+++ src/performance-tests/java/costOfKotlin/PrimitivesTests.kt
@@ -16,30 +16,31 @@ class PrimitivesTests {
     }
 
     @Test
-    fun `kotlin sum is not detectable`() {
-        assertThat(KotlinPrimitives::_1_baseline, !probablyDifferentTo(KotlinPrimitives::_2_sum))
+    fun `kotlin sum is marginally detectable`() {
+        assertThat(KotlinPrimitives::_1_baseline, probablyDifferentTo(KotlinPrimitives::_2_sum))
+        assertThat(KotlinPrimitives::_1_baseline, !probablyFasterThan(KotlinPrimitives::_2_sum))
     }
 
     @Test
-    fun `sum nullable is slower`() {
-        assertThat(KotlinPrimitives::_2_sum, probablyFasterThan(KotlinPrimitives::_3_sum_nullable_bang_bang))
-        assertThat(KotlinPrimitives::_2_sum, !probablyFasterThan(KotlinPrimitives::_3_sum_nullable_bang_bang, byAFactorOf = 0.001))
+    fun `sum nullable is slower than sum`() {
+        assertThat(KotlinPrimitives::_2_sum, probablyFasterThan(KotlinPrimitives::_3_sum_nullable_bang_bang, byMoreThan = 0.01, butNotMoreThan = 0.05))
     }
 
     @Test
-    fun `sum always null is slower`() {
-        assertThat(KotlinPrimitives::_2_sum, probablyFasterThan(KotlinPrimitives::_5_sum_elvis_always_null))
-        assertThat(KotlinPrimitives::_2_sum, !probablyFasterThan(KotlinPrimitives::_5_sum_elvis_always_null, byAFactorOf = 0.001))
+    fun `sum elvis never null is slower than sum`() {
+        assertThat(KotlinPrimitives::_2_sum, probablyFasterThan(KotlinPrimitives::_4_sum_elvis_never_null, byMoreThan = 0.01, butNotMoreThan = 0.05))
     }
 
     @Test
-    fun `branch_prediction is undetectable_50_50`() {
-        assertThat(KotlinPrimitives::_5_sum_elvis_always_null, !probablyDifferentTo(KotlinPrimitives::_6_sum_elvis_50_50_nullable))
+    fun `sum elvis always null is slower than sum but faster than never null`() {
+        assertThat(KotlinPrimitives::_2_sum, probablyFasterThan(KotlinPrimitives::_5_sum_elvis_always_null, byMoreThan = 0.01, butNotMoreThan = 0.05))
+        assertThat(KotlinPrimitives::_5_sum_elvis_always_null, probablyFasterThan(KotlinPrimitives::_4_sum_elvis_never_null, byMoreThan = 0.01, butNotMoreThan = 0.05))
     }
 
     @Test
-    fun `branch_prediction is undetectable_90_10`() {
-        assertThat(KotlinPrimitives::_5_sum_elvis_always_null, !probablyDifferentTo(KotlinPrimitives::_7_sum_elvis_90_10_nullable))
+    fun `random null is worst`() {
+        assertThat(KotlinPrimitives::_4_sum_elvis_never_null, probablyFasterThan(KotlinPrimitives::_6_sum_elvis_50_50_nullable))
+//        assertThat(KotlinPrimitives::_4_sum_elvis_never_null, probablyFasterThan(KotlinPrimitives::_7_sum_elvis_90_10_nullable))
     }
 
 }
