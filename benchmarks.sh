f9fd26049e07b0ea8d36e773df19dd7ed5bd8aa6
Duncan McGregor <duncan@oneeyedmen.com>
2017-10-26 22:16:39 +0100

    Update with new batches

--- benchmarks.sh
+++ benchmarks.sh
@@ -4,11 +4,12 @@ for i in `seq 1 100`;
 do
     NAME=run$i
     echo $NAME
-    ./benchmark.sh -f 1 -wi 20 -i 500 -o $NAME baselines
-    ./benchmark.sh -f 1 -wi 20 -i 500 -o $NAME primitives
-    ./benchmark.sh -f 1 -wi 20 -i 500 -o $NAME strings
-    ./benchmark.sh -f 1 -wi 20 -i 500 -o $NAME invoking
-    ./benchmark.sh -f 1 -wi 20 -i 500 -o $NAME mapping
-    ./benchmark.sh -f 1 -wi 20 -i 500 -o $NAME let
-    ./benchmark.sh -f 1 -wi 20 -i 500 -o $NAME sets
+     ./benchmark.sh -f 1 -wi 20 -i 500 -o $NAME mapLike
+     ./benchmark.sh -f 1 -wi 20 -i 500 -o $NAME baselines
+     ./benchmark.sh -f 1 -wi 20 -i 500 -o $NAME primitives
+     ./benchmark.sh -f 1 -wi 20 -i 500 -o $NAME strings
+     ./benchmark.sh -f 1 -wi 20 -i 500 -o $NAME invoking
+     ./benchmark.sh -f 1 -wi 20 -i 500 -o $NAME mapping
+     ./benchmark.sh -f 1 -wi 20 -i 500 -o $NAME let
+     ./benchmark.sh -f 1 -wi 20 -i 500 -o $NAME properties
 done
