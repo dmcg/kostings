8901566274dad18d1edfd3571270ea4cf3d61dae
Duncan McGregor <duncan@oneeyedmen.com>
2017-10-28 15:04:58 +0100

    More tests for Properties

--- src/performance-tests/java/costOfKotlin/PropertiesTests.kt
+++ src/performance-tests/java/costOfKotlin/PropertiesTests.kt
@@ -1,7 +1,6 @@
 package costOfKotlin
 
 import com.natpryce.hamkrest.assertion.assertThat
-import com.oneeyedmen.kostings.notProvablyFasterThan
 import com.oneeyedmen.kostings.probablyDifferentTo
 import com.oneeyedmen.kostings.probablyFasterThan
 import costOfKotlin.properties.JavaProperties
@@ -10,22 +9,24 @@ import org.junit.Test
 
 class PropertiesTests {
 
-@Test
-fun java() {
-    assertThat(JavaProperties::field_access, ! probablyDifferentTo(JavaProperties::getter))
-    assertThat(JavaProperties::field_access,  notProvablyFasterThan(JavaProperties::method_access))
-}
+    @Test
+    fun java() {
+        assertThat(JavaProperties::field_access,
+            ! probablyDifferentTo(JavaProperties::getter))
+        assertThat(JavaProperties::method_access,
+            probablyFasterThan(JavaProperties::field_access, byMoreThan = 0.0002, butNotMoreThan = 0.0005))
+    }
 
-@Test
-fun kotlin() {
-    assertThat(KotlinProperties::method_property, ! probablyDifferentTo(KotlinProperties::constant_method))
-    assertThat(KotlinProperties::field_property, probablyFasterThan(KotlinProperties::method_property))
-}
+    @Test
+    fun kotlin() {
+        assertThat(KotlinProperties::method_property, ! probablyDifferentTo(KotlinProperties::constant_method))
+        assertThat(KotlinProperties::method_property, ! probablyDifferentTo(JavaProperties::method_access))
 
-@Test
-fun mixed() {
-    assertThat(KotlinProperties::field_property, probablyFasterThan(JavaProperties::field_access))
-    assertThat(KotlinProperties::field_property, probablyFasterThan(JavaProperties::getter))
-}
+        assertThat(KotlinProperties::field_property, probablyFasterThan(JavaProperties::field_access, byMoreThan = 0.015, butNotMoreThan = 0.02))
+        assertThat(KotlinProperties::field_property, probablyFasterThan(JavaProperties::getter, byMoreThan = 0.015, butNotMoreThan = 0.02))
+        assertThat(KotlinProperties::field_property, probablyFasterThan(JavaProperties::method_access, byMoreThan = 0.015, butNotMoreThan = 0.02))
+        assertThat(KotlinProperties::field_property, probablyFasterThan(KotlinProperties::method_property, byMoreThan = 0.015, butNotMoreThan = 0.02))
+        assertThat(KotlinProperties::field_property, probablyFasterThan(KotlinProperties::constant_method, byMoreThan = 0.015, butNotMoreThan = 0.02))
+    }
 
 }
